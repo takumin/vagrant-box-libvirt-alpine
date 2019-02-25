54840a5dd39cdeacf9594ee9a0dc4ffd841cd7bd
takumin <takumiiinn@gmail.com>
2019-02-25 12:47:18 +0900

    fix

--- build.sh
+++ build.sh
@@ -110,8 +110,8 @@ export TEMP_DIR="${TEMP_DIR}"
 ./alpine-chroot-install/alpine-chroot-install
 
 # Install Bios Boot Recode
-${CHROOT_DIR}/enter-chroot grub-install --recheck --target=i386-pc "${BLOCK_DEV}"
-${CHROOT_DIR}/enter-chroot grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --recheck --root-directory="${CHROOT_DIR}" --target=i386-pc "${BLOCK_DEV}"
+# grub-mkconfig -o /boot/grub/grub.cfg
 
 # Unmount RootFs
 awk '{print $2}' /proc/mounts | grep -s "${CHROOT_DIR}" | sort -r | xargs --no-run-if-empty umount
